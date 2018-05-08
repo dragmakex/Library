007f9e9fbf7917caa9885bd01fe59126d7a246fb
Alex <dr4gm4k3x@protonmail.com>
2018-05-08 02:22:10 -0400

    Initial commit

---
+++ Library/frmLibrary.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmLibrary
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lstRatings = New System.Windows.Forms.ListBox()
+        Me.lstBooks = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lstRatings
+        '
+        Me.lstRatings.FormattingEnabled = True
+        Me.lstRatings.Location = New System.Drawing.Point(35, 59)
+        Me.lstRatings.Name = "lstRatings"
+        Me.lstRatings.Size = New System.Drawing.Size(205, 446)
+        Me.lstRatings.TabIndex = 0
+        '
+        'lstBooks
+        '
+        Me.lstBooks.FormattingEnabled = True
+        Me.lstBooks.Location = New System.Drawing.Point(264, 60)
+        Me.lstBooks.Name = "lstBooks"
+        Me.lstBooks.Size = New System.Drawing.Size(487, 446)
+        Me.lstBooks.TabIndex = 1
+        '
+        'frmLibrary
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(832, 610)
+        Me.Controls.Add(Me.lstBooks)
+        Me.Controls.Add(Me.lstRatings)
+        Me.Name = "frmLibrary"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstRatings As ListBox
+    Friend WithEvents lstBooks As ListBox
+End Class
---
+++ Library/frmLibrary.vb
@@ -0,0 +1,92 @@
+﻿Public Class frmLibrary
+    Dim lbr As Library
+    Dim rdr As Readerslist
+
+
+    Structure Readers
+        Dim name As String
+        Dim arrratings() As String
+
+        Function getrstring() As String
+            getrstring = name + " " + arrratings(86)
+        End Function
+    End Structure
+
+    Structure Readerslist
+        Dim arrreaders() As Readers
+
+
+
+        Sub getreaders()
+            Dim path As String
+            Dim i As Integer
+            Dim strline As String
+            ReDim arrreaders(86)
+            Dim rc As Integer
+
+
+            path = "D:\Locker\Files\Projects\CP II\VB.NET\Library\BookResources\readerratings.txt"
+
+            FileOpen(1, path, OpenMode.Input)
+
+            Do While Not EOF(1)
+                strline = Trim(LineInput(1))
+                For i = 1 To Len(strline)
+                    arrreaders(rc).name = LineInput(1)
+                    arrreaders(rc).arrratings(86) = LineInput(1)
+                    frmLibrary.lstRatings.Items.Add(arrreaders(rc).getrstring)
+                    rc = rc = 1
+                Next
+            Loop
+
+        End Sub
+    End Structure
+    Structure Book
+        Dim author As String
+        Dim title As String
+
+        Function getbstr() As String
+            getbstr = author + " " + title
+        End Function
+    End Structure
+    Structure Library
+        Dim arrbooks() As Book
+
+        Sub getbooks()
+            Dim path As String
+            Dim i As Integer
+            Dim strline As String
+            ReDim arrbooks(55)
+            Dim bc As Integer
+
+            path = "D:\Locker\Files\Projects\CP II\VB.NET\Library\BookResources\books.txt"
+            FileOpen(1, path, OpenMode.Input)
+
+            Do While Not EOF(1)
+                strline = Trim(LineInput(1))
+                For i = 1 To Len(strline)
+                    If Mid(strline, i, 1) = "," Then
+                        arrbooks(bc).author = Mid(strline, 1, i - 1)
+                        arrbooks(bc).title = Mid(strline, i + 1, Len(strline))
+                        frmLibrary.lstBooks.Items.Add(arrbooks(bc).getbstr)
+                        bc = bc + 1
+                    End If
+                Next
+
+            Loop
+            FileClose(1)
+        End Sub
+    End Structure
+    Sub fill() 'display readers and books
+        lbr.getbooks()
+        rdr.getreaders()
+
+    End Sub
+    Private Sub frmLibrary_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        fill()
+    End Sub
+
+    Private Sub cmdFill_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
